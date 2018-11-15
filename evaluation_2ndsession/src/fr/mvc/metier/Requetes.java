227af7f76bac40442a17d7cb9e241d68b8935615
GORCE Pierre <looupsing@gmail.com>
2018-11-15 23:21:14 +0100

    cleaning

--- evaluation_2ndsession/src/fr/mvc/metier/Requetes.java
+++ evaluation_2ndsession/src/fr/mvc/metier/Requetes.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import fr.mvc.model.*;
-import fr.mvc.connection.AccesBD;;
+import fr.mvc.connection.AccesBD;
 
 public class Requetes {
 
